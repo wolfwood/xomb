edcaa8a243d1c9f545bff1abedf1d88ea6f8232a
wilkie05 <wilkie05>
2008-08-09 18:32:34 +0000

    Added log mechanism thing.

---
+++ src/kernel/log.d
@@ -0,0 +1,34 @@
+module kernel.log;
+
+
+import kernel.dev.vga;
+
+const char[] spaces = "...........................................................................";
+
+void printLogLine(char[] string)
+{
+	Console.setColors(Color.White, Color.Black);
+	kprintf!("  .  {} {} [ ")(string, spaces[0..65-string.length]);
+	Console.setColors(Color.Yellow, Color.Black);
+	kprintf!(".. ")();
+	Console.setColors(Color.White, Color.Black);
+	kprintf!("]")();
+}
+
+void printLogSuccess()
+{
+	int x,y;
+	Console.getPosition(x,y);
+	Console.setPosition(x-5,y);
+	Console.setColors(Color.HighGreen, Color.Black);
+	kprintfln!(" OK ")();
+}
+
+void printLogFail()
+{
+	int x,y;
+	Console.getPosition(x,y);
+	Console.setPosition(x-5,y);
+	Console.setColors(Color.HighRed, Color.Black);
+	kprintfln!("FAIL")();
+}
