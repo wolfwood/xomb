e18d2238b0dacd3424c341a539a8e1314d939945
wilkie <wilkie05@gmail.com>
2008-10-30 01:36:30 -0400

    Just committing some changes to some architecture things, like accessing MSRs which has now become very important and useful.

--- src/kernel/arch/x86_64/init.d
+++ src/kernel/arch/x86_64/init.d
@@ -24,7 +24,7 @@ static:
 		Returns:
 			The result of the CPUID instruction for the given function.
 	*/
-	uint cpuid(uint func)
+	uint cpuidDX(uint func)
 	{
 		asm
 		{
@@ -32,6 +32,76 @@ static:
 			"movl %%edi, %%eax";
 			"cpuid";
 			"movl %%edx, %%eax";
+			"retq";
+		}
+	}
+
+	uint cpuidAX(uint func)
+	{
+		asm
+		{
+			naked;
+			"movl %%edi, %%eax";
+			"cpuid";
+			"retq";
+		}
+	}
+
+	uint cpuidBX(uint func)
+	{
+		asm
+		{
+			naked;
+			"movl %%edi, %%eax";
+			"cpuid";
+			"movl %%ebx, %%eax";
+			"retq";
+		}
+	}
+
+	uint cpuidCX(uint func)
+	{
+		asm
+		{
+			naked;
+			"movl %%edi, %%eax";
+			"cpuid";
+			"movl %%ecx, %%eax";
+			"retq";
+		}
+	}
+
+	// Will read a MSR (Model-Specific-Register)
+	ulong readMSR(uint MSR)
+	{
+		asm
+		{
+			naked;
+			// move the MSR index to $ECX
+			"movl %%edi, %%ecx";
+			// read the MSR
+			"rdmsr";
+			// the D ABI returns ulong as the HI in $EDX and the LO in $EAX
+			// this is the same as what is set by the processor
+			"retq";
+		}
+	}
+
+	void writeMSR(uint MSR, ulong value)
+	{
+		asm
+		{
+			naked;
+			// move the MSR index to $ECX
+			"movl %%edi, %%ecx";
+			// move the value to its perspective registers
+			// HI : $EDX
+			// LO : $EAX
+			"movl %%esi, %%eax";
+			"mov 32, %%cl";
+			"sar %%cl, %%rsi";
+			"movl %%esi, %%edx";
+			"wrmsr";
 			"retq";
 		}
 	}
@@ -62,26 +132,41 @@ static:
 	{	
 		printLogLine("Validating CPU Functionality");
 
-		if(!(cpuid(0x8000_0001) & 0b1000_0000_0000))
+		// check for SYSCALL
+		if(!(cpuidDX(0x8000_0001) & 0b1000_0000_0000))
 		{
 			printLogFail();
 			kprintfln!("-- Your computer is not cool enough, we need SYSCALL and SYSRET.")();
 			asm { cli; hlt; }
 		}
+
+		// check for x2APIC
+		if(!(cpuidAX(0x1) & (1 << 21)))
+		{
+			printLogFail();
+			kprintfln!("-- Your computer is not cool enough, we need x2APIC")();
+			asm { cli; hlt; }
+		}
+
+		// we have total success
 		printLogSuccess();
 	}
 
 	void disableInterrupts()
 	{
 		asm {
-			cli;
+			naked;
+			"cli";
+			"retq";
 		}
 	}
 
 	void enableInterrupts()
 	{
 		asm {
-			sti;
+			naked;
+			"sti";
+			"retq";
 		}
 	}
 }
