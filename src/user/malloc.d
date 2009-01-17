4eb80a77e3adf533f508dc176e3921a89d2b90c5
Lindsey <hazardous.narayan@gmail.com>
2009-01-17 14:23:42 +0800

    up to date linked list malloc with printed debug statments all removed. Adding and removing works kinda funny right now as one adds to the end of the list and the other to the beginning but that really doesn't matter. I figure this will all eventually be replaced with a more elegant implementation of malloc in the future.
    Signed-off-by: xomboverlord <overlord@xomb.org>

--- src/user/malloc.d
+++ src/user/malloc.d
@@ -1,7 +1,7 @@
 // malloc - dynamic memory allocation
 
 // It is hard enough to remember my opinions, without also remembering my reasons for them!
-//        -Friedrich Nietzsche
+//				-Friedrich Nietzsche
 
 import user.syscall;
 import user.basicio;
@@ -9,221 +9,204 @@ import user.constants;
 
 struct header
 {
-  header *next;
-  header *prev;
-  header *nextFree; // only used by items in the freelist
-  header *prevFree; // only used by items in the freelist
-  long chunkSize;
+	header *next;
+	header *prev;
+	header *nextFree; // only used by items in the freelist
+	header *prevFree; // only used by items in the freelist
+	long chunkSize;
 }
 
 ulong unusedSpace = 0;
 
 header *allocHead = null;
 header *allocTail = null;
-header *freeHead  = null;
-header *freeTail  = null;
+header *freeHead	= null;
+header *freeTail	= null;
 
 // allocates requested memory
 void* malloc(long size)
 {
-  header *cur = freeHead;
+	header *cur = freeHead;
 
-  // check the free list for available chunks
-  while(cur !is null)
-  {
-    // free chunks have their size as negative
-    if(-cur.chunkSize >= size)
-    {
-      // use entire chunk
-      if(-cur.chunkSize - size <= header.sizeof)
-      {
-        if (cur is freeHead)
-          freeHead = cur.next;
-        if (cur is freeTail)
-          freeTail = cur.prev;
+	// check the free list for available chunks
+	while(cur !is null)
+	{
+		// free chunks have their size as negative
+		if(-cur.chunkSize >= size)
+		{
+			// use entire chunk
+			if(-cur.chunkSize - size <= header.sizeof)
+			{
+				if (cur is freeHead)
+					freeHead = cur.next;
+				if (cur is freeTail)
+					freeTail = cur.prev;
 
-        // remove from free list
-        if (cur.prevFree !is null)
-          cur.prevFree.nextFree = cur.nextFree;
-        if (cur.nextFree !is null)
-          cur.nextFree.prevFree = cur.prevFree;
+				// remove from free list
+				if (cur.prevFree !is null)
+					cur.prevFree.nextFree = cur.nextFree;
+				if (cur.nextFree !is null)
+					cur.nextFree.prevFree = cur.prevFree;
 
-        cur.chunkSize = -cur.chunkSize;
+				cur.chunkSize = -cur.chunkSize;
 
-        return (cur + 1);  // return pointer to where chunk begins after header
-      }
-      else
-      {
-        // create new chunk
-        header *newHeader = cast(header*)(cast(ubyte*)(cur + 1) + size);
+				return (cur + 1);	// return pointer to where chunk begins after header
+			}
+			else
+			{
+			    // create new chunk
+				header *newHeader = cast(header*)(cast(ubyte*)(cur + 1) + size);
 
-        // setup the header
-        newHeader.chunkSize = cur.chunkSize + size + header.sizeof;
-        newHeader.prev = cur;
-        newHeader.next = cur.next;
+				// setup the header
+				newHeader.chunkSize = cur.chunkSize + size + header.sizeof;
+				newHeader.prev = cur;
+				newHeader.next = cur.next;
 
-        // add into list
-        if(cur.next !is null)
-          cur.next.prev = newHeader;
+				// add into list
+				if(cur.next !is null)
+					cur.next.prev = newHeader;
 
-        cur.next = newHeader;
+				cur.next = newHeader;
 
-        // resize chunk
-        cur.chunkSize = size;
+				// resize chunk
+				cur.chunkSize = size;
 
-        // add new chunk to free list
-        newHeader.nextFree = cur.nextFree;
-        newHeader.prevFree = cur.prevFree;
+				// add new chunk to free list
+				newHeader.nextFree = cur.nextFree;
+				newHeader.prevFree = cur.prevFree;
 
-        if(cur is freeHead)
-          freeHead = newHeader;
-        if(cur is freeTail)
-          freeTail = newHeader;
+				if(cur is freeHead)
+					freeHead = newHeader;
+				if(cur is freeTail)
+					freeTail = newHeader;
 
-        // remove current chunk from free list
-        if (cur.prevFree !is null)
-          cur.prevFree.nextFree = newHeader;
-        if (cur.nextFree !is null)
-          cur.prevFree.nextFree = newHeader;
+				// remove current chunk from free list
+				if (cur.prevFree !is null)
+					cur.prevFree.nextFree = newHeader;
+				if (cur.nextFree !is null)
+					cur.prevFree.nextFree = newHeader;
 
-        // return the chunk
-        return (cur + 1);
-      }
-    }
+				// return the chunk
+				return (cur + 1);
+			}
+		}
 
-    cur = cur.next;
-  }
+		cur = cur.next;
+	}
 
-  header* newHeader;
-  if (allocTail is null)
-  {
-    newHeader = cast(header*)allocPage();
-    unusedSpace += Kernel.PAGE_SIZE;
+	header* newHeader;
+	if (allocTail is null)
+	{
+		newHeader = cast(header*)allocPage();
+		unusedSpace += Kernel.PAGE_SIZE;
+	}
+	else
+	{
+		newHeader = allocTail;
+	}
 
-    print("had to get new stuff\n");
-  }
-  else
-  {
-    newHeader = allocTail;
-  }
+	while(unusedSpace < header.sizeof + size)
+	{
+		if(allocPage() == null)
+			return null;
 
-  print("allocating ", size, " bytes!\n");
+		unusedSpace += Kernel.PAGE_SIZE;
+	}
 
-  while(unusedSpace < header.sizeof + size)
-  {
-    if(allocPage() == null)
-      return null;
+	// setup the page
+	newHeader.next = null;
+	newHeader.prev = allocTail;
+	newHeader.chunkSize = size;
 
-    unusedSpace += Kernel.PAGE_SIZE;
-  }
+	if (allocTail !is null)
+		allocTail.next = newHeader;
 
-  // setup the page
-  newHeader.next = null;
-  newHeader.prev = allocTail;
-  newHeader.chunkSize = size;
+	allocTail = newHeader;
 
-  if (allocTail !is null)
-    allocTail.next = newHeader;
+	if(allocHead is null)
+		allocHead = newHeader;
 
-  allocTail = newHeader;
+	// take it out of unused space
+	unusedSpace -= size + header.sizeof;
 
-  if(allocHead is null)
-    allocHead = newHeader;
-
-  // take it out of unused space
-  unusedSpace -= size + header.sizeof;
-
-  return (newHeader + 1);
+	return (newHeader + 1);
 }
 
 // frees that was allocated
 void free(void *pointer)
 {
-  header *freeHeader = (cast(header*)pointer) - 1;
+	header *freeHeader = (cast(header*)pointer) - 1;
 
-  print("freeing block with size ", freeHeader.chunkSize, "\n");
+	freeHeader.chunkSize = -freeHeader.chunkSize;
 
-  freeHeader.chunkSize = -freeHeader.chunkSize;
+	// add to freelist
+	if(freeHead !is null)
+	{
+		freeHeader.nextFree = freeHead.nextFree;
+		freeHead.nextFree = freeHeader;
+	}
+	else
+	{
+		freeHeader.nextFree = freeHeader;
+	}
 
-  // add to freelist
-  if(freeHead !is null)
-  {
-    freeHeader.nextFree = freeHead.nextFree;
-    freeHead.nextFree = freeHeader;
-  }
-  else
-  {
-    freeHeader.nextFree = freeHeader;
-  }
+	freeHeader.prevFree = freeHeader;
+	freeHead = freeHeader;
 
-  freeHeader.prevFree = freeHeader;
-  freeHead = freeHeader;
+	// merge blocks
+	if(freeHeader.prev !is null && freeHeader.prev.chunkSize < 0)
+	{
+		freeHeader.prev.chunkSize = freeHeader.chunkSize + header.sizeof;
+		freeHeader.prev.next = freeHeader.next;
 
-  // merge blocks
-  if(freeHeader.prev !is null && freeHeader.prev.chunkSize < 0)
-  {
-    freeHeader.prev.chunkSize = freeHeader.chunkSize + header.sizeof;
-    freeHeader.prev.next = freeHeader.next;
+		if(freeHeader.next !is null)
+		{
+			freeHeader.next.prev = freeHeader.prev;
+		}
 
-    if(freeHeader.next !is null)
-    {
-      freeHeader.next.prev = freeHeader.prev;
-    }
+		freeHeader = freeHeader.prev;
+	}
 
-    print("merged with left block\n");
+	if(freeHeader.next !is null && freeHeader.next.chunkSize > 0)
+	{
+		freeHeader.chunkSize = freeHeader.next.chunkSize + header.sizeof;
 
-    freeHeader = freeHeader.prev;
-  }
+		if(freeHeader.next.next !is null)
+		{
+			freeHeader.next.next.prev = freeHeader;
+		}
 
-  if(freeHeader.next !is null && freeHeader.next.chunkSize > 0)
-  {
-    freeHeader.chunkSize = freeHeader.next.chunkSize + header.sizeof;
+		freeHeader.next = freeHeader.next.next;
+	}
 
-    if(freeHeader.next.next !is null)
-    {
-      freeHeader.next.next.prev = freeHeader;
-    }
+	if (freeHeader.next is null)
+	{
+		// remove this from all lists
+		if (freeHeader.nextFree !is null)
+				freeHeader.nextFree.prevFree = freeHeader.prevFree;
 
-    print("merged with right block\n");
+		if (freeHeader.prevFree !is null)
+				freeHeader.prevFree.nextFree = freeHeader.nextFree;
 
-    freeHeader.next = freeHeader.next.next;
-  }
+		if (freeHeader is freeHead)
+		{
+				freeHead = freeHeader.prevFree;
+		}
 
-  if (freeHeader.next is null)
-  {
-    print("removed last block\n");
+		// remove from alloc list
+		if (freeHeader.prev !is null)
+				freeHeader.prev.next = null;
 
-    // remove this from all lists
-    if (freeHeader.nextFree !is null)
-        freeHeader.nextFree.prevFree = freeHeader.prevFree;
+		allocTail = freeHeader.prev;
 
-    if (freeHeader.prevFree !is null)
-        freeHeader.prevFree.nextFree = freeHeader.nextFree;
+		// we have more unused space
+		unusedSpace += (-freeHeader.chunkSize + header.sizeof);
 
-    if (freeHeader is freeHead)
-    {
-        freeHead = freeHeader.prevFree;
-    }
-
-    // remove from alloc list
-    if (freeHeader.prev !is null)
-        freeHeader.prev.next = null;
-
-    allocTail = freeHeader.prev;
-
-    print("unused space: ", unusedSpace, "\n");
-
-    // we have more unused space
-    unusedSpace += (-freeHeader.chunkSize + header.sizeof);
-
-    print("unused space: ", unusedSpace, "\n");
-
-    // free the pages if possible
-    while(unusedSpace >= Kernel.PAGE_SIZE)
-    {
-        print("freeing a page\n");
-        freePage();
-        unusedSpace -= Kernel.PAGE_SIZE;
-    }
-  }
+		// free the pages if possible
+		while(unusedSpace >= Kernel.PAGE_SIZE)
+		{
+				freePage();
+				unusedSpace -= Kernel.PAGE_SIZE;
+		}
+	}
 }
