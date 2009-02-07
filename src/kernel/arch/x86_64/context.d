4cdcb5cceaf4c42d7cfe11c5fbfd7c9764808310
wilkie <wilkie05@gmail.com>
2009-02-06 21:29:01 -0500

    more operands flipped

--- src/kernel/arch/x86_64/context.d
+++ src/kernel/arch/x86_64/context.d
@@ -7,24 +7,24 @@ import kernel.core.util;
 
 Template: contextSwitchSave, contextSwitchRestore
 
-The following templates have been create to handle the code that is required to 
-context switch saves and restores. 
+The following templates have been create to handle the code that is required to
+context switch saves and restores.
 
 At first we had originally tried to just create functions that could be called
 for these operations however, there are number of tricky things you have to worry
-about when executing this code from a function. 
+about when executing this code from a function.
 
 By doing this in templates allows us to store this code in one place and easily
-call it by using an inline mixin() call. 
+call it by using an inline mixin() call.
 
 For example:
 
 function blah()
 {
 	mixin(!contextSwitchSave());
-	
+
 	asm { .... }
-	
+
 	mixin(!contextSwitchRestore());
 }
 
@@ -35,21 +35,21 @@ template contextSwitchSave()
 	asm
 	{
 		naked;
-		"pushq %%rax";
-		"pushq %%rbx";
-		"pushq %%rcx";
-		"pushq %%rdx";
-		"pushq %%rsi";
-		"pushq %%rdi";
-		"pushq %%rbp";
-		"pushq %%r8";
-		"pushq %%r9";
-		"pushq %%r10";
-		"pushq %%r11";
-		"pushq %%r12";
-		"pushq %%r13";
-		"pushq %%r14";
-		"pushq %%r15";
+		pushq %%rax;
+		pushq %%rbx;
+		pushq %%rcx;
+		pushq %%rdx;
+		pushq %%rsi;
+		pushq %%rdi;
+		pushq %%rbp;
+		pushq %%r8;
+		pushq %%r9;
+		pushq %%r10;
+		pushq %%r11;
+		pushq %%r12;
+		pushq %%r13;
+		pushq %%r14;
+		pushq %%r15;
 	}
 	`;
 }
@@ -60,21 +60,21 @@ template contextSwitchRestore()
 	asm
 	{
 		naked;
-		"popq %%r15";
-		"popq %%r14";
-		"popq %%r13";
-		"popq %%r12";
-		"popq %%r11";
-		"popq %%r10";
-		"popq %%r9";
-		"popq %%r8";
-		"popq %%rbp";
-		"popq %%rdi";
-		"popq %%rsi";
-		"popq %%rdx";
-		"popq %%rcx";
-		"popq %%rbx";
-		"popq %%rax";
+		popq %%r15;
+		popq %%r14;
+		popq %%r13;
+		popq %%r12;
+		popq %%r11;
+		popq %%r10;
+		popq %%r9;
+		popq %%r8;
+		popq %%rbp;
+		popq %%rdi;
+		popq %%rsi;
+		popq %%rdx;
+		popq %%rcx;
+		popq %%rbx;
+		popq %%rax;
 	}
 	`;
 }
@@ -84,8 +84,9 @@ template contextSwitchStack()
 	const char[] contextSwitchStack = `
 
 		asm {
-	
-			"movq $` ~ Itoa!(vMem.REGISTER_STACK) ~ `, %%rsp";
+
+			//movq $` ~ Itoa!(vMem.REGISTER_STACK) ~ `, %%rsp;
+			movq rsp, vMem.REGISTER_STACK;
 
 		}
 
@@ -104,48 +105,59 @@ template contextSwitchPrepare(char[] address)
 		asm {
 			naked;
 
-			"movq %%rsp, %%rcx";
-
-			// "movq %0, %%rbx" :: "m" ` ~ address ~ ` : "rbx";
+			//"movq %%rsp, %%rcx";
+			movq rcx, rsp;
 
 			// switch to stack
 
-			"movq $` ~ Itoa!(vMem.REGISTER_STACK) ~ `, %%rsp";
+			//"movq $` ~ Itoa!(vMem.REGISTER_STACK) ~ `, %%rsp";
+			movq rsp, vMem.REGISTER_STACK;
 
 			// stack stuff
 
 			// SS
-			"pushq $((8 << 3) | 3)";
+			//"pushq $((8 << 3) | 3)";
+			pushq ((8 << 3) | 3);
 
 			// RSP (of environment)
-			"movq $` ~ Itoa!(vMem.ENVIRONMENT_STACK) ~ `, %%rax";
-			"pushq %%rax";
+			//"movq $` ~ Itoa!(vMem.ENVIRONMENT_STACK) ~ `, %%rax";
+			movq rax, vMem.ENVIRONMENT_STACK;
+			//"pushq %%rax";
+			pushq rax;
 
 			// FLAGS
-			"pushq $((1 << 9) | (3 << 12))";
+			//"pushq $((1 << 9) | (3 << 12))";
+			pushq ((1 << 9) | (3 << 12));
 
 			// CS
-			"pushq $((9 << 3) | 3)";
+			//"pushq $((9 << 3) | 3)";
+			pushq ((9 << 3) | 3);
 
 			// RIP
-			//"pushq %%rbx";
-			"pushq %%rdi";
+			//"pushq %%rdi";
+			pushq rdi;
 
 			// EMULATE ERROR CODE, INTERRUPT VECTOR NUMBER
-			"pushq $0";
-			"pushq $0";
-			
+			//"pushq $0";
+			//"pushq $0";
+			pushq 0;
+			pushq 0;
+
 		}
 
 		mixin(contextSwitchSave!());
 
 		asm {
 
-			"movq %%rsp, %%rax; movq %%rax, ` ~ Itoa!(vMem.REGISTER_STACK_POS) ~ `";
+			//"movq %%rsp, %%rax; movq %%rax, ` ~ Itoa!(vMem.REGISTER_STACK_POS) ~ `";
+			movq rax, rsp;
+			movq vMem.REGISTER_STACK_POS, rax;
 
-			"movq %%rcx, %%rsp";
+			//"movq %%rcx, %%rsp";
+			movq rsp, rcx;
 
-			"ret";
+			//"ret";
+			ret;
 
 		}
 
