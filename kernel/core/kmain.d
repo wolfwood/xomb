c299f9d169ff3fe5ea1ee257f6e4da2aaa5781d4
Steve Klabnik <steve@steveklabnik.com>
2009-02-21 16:20:42 -0500

    Initial creation of the XOmB Bare Bones distribution

---
+++ kernel/core/kmain.d
@@ -0,0 +1,13 @@
+/* XOmB Bare Bones
+ *
+ * This is the bare minimum needed for an OS written in the D language.
+ *
+ */
+
+module kernel.core.kmain;
+
+extern(C) void kmain(int flags, void *data) {
+
+  for(;;) {}
+
+}
