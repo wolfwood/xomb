7f128f42d544472b4657a124bb48d82271dae3c2
wilkie <wilkie05@gmail.com>
2009-05-03 22:50:37 -0400

    Fixed arch selection. Added single file typeinfo fix.

--- kernel/arch/select.d
+++ kernel/arch/select.d
@@ -3,5 +3,5 @@ module kernel.arch.select;
 // Import the architecture interfaces
 // This is found in the kernel/arch/ARCHLABEL/imports/ folder
 // This will be copied to the dsss_imports directory
-public import archimport;
+public import architecture;
 
--- kernel/arch/x86_64/imports/architecture.d
+++ kernel/arch/x86_64/imports/architecture.d
@@ -1,12 +1,12 @@
 /*
- * archimport.d
+ * architecture.d
  *
  * This file will publically import all modules used to define
  * the architecture interfaces provided by this architecture.
  *
  */
 
-module archimport;
+module architecture;
 
 public import kernel.arch.x86_64.main;
 public import kernel.arch.x86_64.cpu;
