d3e570bb30ba2c02b30d7caa49ca50a747dedafd
Dave Wilkinson <wilkie05@gmail.com>
2009-09-19 09:00:28 -0400

    Added syscall and D runtime static libraries for C applications to bind with D facilities. The C test app calls a D system call.

--- app/c/testapp.c
+++ app/c/testapp.c
@@ -1,3 +1,6 @@
+#include <syscall.h>
+
 void main() {
+	int foo = add(3,4);
 	for(;;) {}
 }
--- user/c/syscall.c
+++
@@ -1,6 +0,0 @@
-int nativeSyscall(unsigned int ID, void* ret, void* params);
-
-int add(int a, int b);
-
-struct ConsoleInfo;
-void requestConsole(struct ConsoleInfo*);
