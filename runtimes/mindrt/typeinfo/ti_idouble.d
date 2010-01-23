cf1b5b98ccdd3813744f261a4b1975d89e4d5b09
wilkie <wilkie05@gmail.com>
2010-01-23 18:45:32 -0500

    Moved TypeInfo structures.

---
+++ runtimes/mindrt/typeinfo/ti_idouble.d
@@ -0,0 +1,12 @@
+
+// idouble
+
+module mindrt.typeinfo.ti_idouble;
+
+private import mindrt.typeinfo.ti_double;
+
+class TypeInfo_p : TypeInfo_d
+{
+    char[] toString() { return "idouble"; }
+}
+
