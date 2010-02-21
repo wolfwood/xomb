40e80757d83beeb91418397d633d29b3e749e618
wilkie <wilkie05@gmail.com>
2010-02-21 12:01:25 +0800

    Moved something not architecture specific.

--- kernel/arch/x86_64/architecture/pci.d
+++ kernel/arch/x86_64/architecture/pci.d
@@ -18,18 +18,4 @@ static:
 	// Description: Will write to PCI.
 	void write() {
 	}
-
-	// Description: This function will verify a bus and slot exist.
-	bool verify(ushort bus, ushort slot) {
-		ushort vendor = read(bus, slot, 0, 0);
-
-		if (vendor == 0xffff) {
-			// no vendor can be all ones
-			// PCI will return all ones on an invalid request
-			// therefore, this bus and slot are invalid
-			return false;
-		}
-
-		return true;
-	}
 }
--- kernel/dev/pci.d
+++ kernel/dev/pci.d
@@ -8,4 +8,17 @@
 module kernel.dev.pci;
 
 class PCI {
+	// Description: This function will verify a bus and slot exist.
+	bool verify(ushort bus, ushort slot) {
+		ushort vendor = read(bus, slot, 0, 0);
+
+		if (vendor == 0xffff) {
+			// no vendor can be all ones
+			// PCI will return all ones on an invalid request
+			// therefore, this bus and slot are invalid
+			return false;
+		}
+
+		return true;
+	}
 }
