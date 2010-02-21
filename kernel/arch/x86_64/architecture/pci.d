862790d28d2f12357537f265b3660a8a3dde92f9
wilkie <wilkie05@gmail.com>
2010-02-21 12:00:51 +0800

    Added some PCI code

--- kernel/arch/x86_64/architecture/pci.d
+++ kernel/arch/x86_64/architecture/pci.d
@@ -8,4 +8,28 @@
 module architecture.pci;
 
 class PCIImplementation {
+static:
+
+	// Description: Will read from PCI.
+	ushort read(ushort bus, ushort slot, ushort func, ushort offset) {
+		return 0;
+	}
+
+	// Description: Will write to PCI.
+	void write() {
+	}
+
+	// Description: This function will verify a bus and slot exist.
+	bool verify(ushort bus, ushort slot) {
+		ushort vendor = read(bus, slot, 0, 0);
+
+		if (vendor == 0xffff) {
+			// no vendor can be all ones
+			// PCI will return all ones on an invalid request
+			// therefore, this bus and slot are invalid
+			return false;
+		}
+
+		return true;
+	}
 }
