d58b343dac2347316c85c746b0630348fdbca36c
wilkie <wilkie05@gmail.com>
2010-02-22 01:39:43 -0500

    Enumerate ALL devices.

--- kernel/dev/pci.d
+++ kernel/dev/pci.d
@@ -144,15 +144,15 @@ package:
 private:
 
 	ubyte read8(ubyte offset) {
-		return PCI.read8(_address | PCI.Offset.HeaderType);
+		return PCI.read8(_address | offset);
 	}
 
 	ushort read16(ubyte offset) {
-		return PCI.read16(_address | PCI.Offset.HeaderType);
+		return PCI.read16(_address | offset);
 	}
 
 	uint read32(ubyte offset) {
-		return PCI.read32(_address | PCI.Offset.HeaderType);
+		return PCI.read32(_address | offset);
 	}
 }
 
@@ -292,15 +292,15 @@ package:
 private:
 
 	ubyte read8(ubyte offset) {
-		return PCI.read8(_address | PCI.Offset.HeaderType);
+		return PCI.read8(_address | offset);
 	}
 
 	ushort read16(ubyte offset) {
-		return PCI.read16(_address | PCI.Offset.HeaderType);
+		return PCI.read16(_address | offset);
 	}
 
 	uint read32(ubyte offset) {
-		return PCI.read32(_address | PCI.Offset.HeaderType);
+		return PCI.read32(_address | offset);
 	}
 }
 
@@ -308,18 +308,18 @@ class PCI : PCIConfiguration {
 static:
 
 	enum Offset : ubyte {
-		DeviceID,
-		VendorID = 0x2,
-		Status = 0x4,
-		Command = 0x6,
-		ClassCode = 0x8,
-		Subclass = 0x9,
-		ProgIF = 0xa,
-		RevisionID = 0xb,
-		BIST = 0xc,
-		HeaderType = 0xd,
-		LatencyTimer = 0xe,
-		CacheLineSize = 0xf,
+		VendorID,
+		DeviceID = 0x2,
+		Command = 0x4,
+		Status = 0x6,
+		RevisionID = 0x8,
+		ProgIF = 0x9,
+		Subclass = 0xa,
+		ClassCode = 0xb,
+		CacheLineSize = 0xc,
+		LatencyTimer = 0xd,
+		HeaderType = 0xe,
+		BIST = 0xf,
 		BaseAddress0 = 0x10,
 		BaseAddress1 = 0x14,
 		BaseAddress2 = 0x18,
@@ -327,51 +327,51 @@ static:
 		BaseAddress4 = 0x20,
 		BaseAddress5 = 0x24,
 		CardbusCISPtr = 0x28,
-		SubsystemID = 0x2c,
-		SubsystemVendorID = 0x2e,
+		SubsystemVendorID = 0x2c,
+		SubsystemID = 0x2e,
 		ExpansionRomBaseAddress = 0x30,
-		CapabilitiesPointer = 0x37,
-		MaxLatency = 0x3c,
-		MinGrant = 0x3d,
-		InterruptPin = 0x3e,
-		InterruptLine = 0x3f
+		CapabilitiesPointer = 0x34,
+		InterruptLine = 0x3c,
+		InterruptPin = 0x3d,
+		MinGrant = 0x3e,
+		MaxLatency = 0x3f
 	}
 
 	enum BridgeOffset : ubyte {
-		DeviceID,
-		VendorID = 0x2,
-		Status = 0x4,
-		Command = 0x6,
-		ClassCode = 0x8,
-		Subclass = 0x9,
-		ProgIF = 0xa,
-		RevisionID = 0xb,
-		BIST = 0xc,
-		HeaderType = 0xd,
-		LatencyTimer = 0xe,
-		CacheLineSize = 0xf,
+		VendorID,
+		DeviceID = 0x2,
+		Command = 0x4,
+		Status = 0x6,
+		RevisionID = 0x8,
+		ProgIF = 0x9,
+		Subclass = 0xa,
+		ClassCode = 0xb,
+		CacheLineSize = 0xc,
+		LatencyTimer = 0xd,
+		HeaderType = 0xe,
+		BIST = 0xf,
 		BaseAddress0 = 0x10,
 		BaseAddress1 = 0x14,
-		SecondaryLatencyTimer = 0x18,
-		SubordinateBusNumber = 0x19,
-		SecondaryBusNumber = 0x1a,
-		PrimaryBusNumber = 0x1b,
-		SecondaryStatus = 0x1c,
-		IOLimit = 0x1e,
-		IOBase = 0x1f,
-		MemoryLimit = 0x20,
-		MemoryBase = 0x22,
-		PrefetchableMemoryLimit = 0x24,
-		PrefetchableMemoryBase = 0x26,
+		PrimaryBusNumber = 0x18,
+		SecondaryBusNumber = 0x19,
+		SubordinateBusNumber = 0x1a,
+		SecondaryLatencyTimer = 0x1b,
+		IOBase = 0x1c,
+		IOLimit = 0x1d,
+		SecondaryStatus = 0x1e,
+		MemoryBase = 0x20,
+		MemoryLimit = 0x22,
+		PrefetchableMemoryBase = 0x24,
+		PrefetchableMemoryLimit = 0x26,
 		PrefetchableBaseUpper32 = 0x28,
 		PrefetchableLimitUpper32 = 0x2c,
-		IOLimitUpper16 = 0x30,
-		IOBaseUpper16 = 0x32,
-		CapabilitiesPointer = 0x37,
+		IOBaseUpper16 = 0x30,
+		IOLimitUpper16 = 0x32,
+		CapabilitiesPointer = 0x34,
 		ExpansionRomBaseAddress = 0x38,
-		BridgeControl = 0x3c,
-		InterruptPin = 0x3e,
-		InterruptLine = 0x3f
+		InterruptLine = 0x3c,
+		InterruptPin = 0x3d,
+		BridgeControl = 0x3e
 	}
 
 	// Description: Will configure and scan the PCI busses.
@@ -396,7 +396,8 @@ static:
 		kprintfln!("Scanning PCI Bus {}")(bus);
 
 		void printDevice() {
-			kprintfln!("PCI Device ID: {} Vendor ID: {}")(current.deviceID, current.vendorID);
+			kprintfln!("PCI Address: {x} Device ID: {x} Vendor ID: {x}")
+				(current.address, current.deviceID, current.vendorID);
 		}
 
 		void checkForBridge() {
@@ -418,17 +419,24 @@ static:
 				// Check the header
 				checkForBridge();
 
-				if ((current.headerType & 0x80) == 0x80) {
+				bool hasFunctions = true;
+
+				/*
+				ubyte busHeaderType = current.headerType;
+				hasFunction = (busHeaderType & 0x80) == 0x80;
+				*/
+
+				if (hasFunctions) {
 					// the header type field will tell us if multiple functions exist
 					// this is true when bit 7 is set
 
 					// Yet again, the functions are limited by the address field layout
+					kprintfln!("Checking functions")();
 					for (uint func = 1; func < 8; func++) {
 						current._address = address(bus, device, func);
-						if (current.vendorID == 0xffff) {
-							break;
+						if (current.vendorID != 0xffff) {
+							checkForBridge();
 						}
-						checkForBridge();
 					}
 				}
 			}
@@ -439,14 +447,14 @@ static:
 	uint address(ushort bus, ushort device, ushort func, ushort offset) {
 		return (cast(uint)bus << 16) | (cast(uint)device << 11)
 				| (cast(uint)func << 8) | (cast(uint)offset & 0xfc)
-				| (cast(uint)0x80000000); // the last value is to set enable bit
+				| (cast(uint)0x80000000);
 	}
 
 	// Description: Will compute the address for a particular device without the offset.
 	uint address(ushort bus, ushort device, ushort func) {
 		return (cast(uint)bus << 16) | (cast(uint)device << 11)
 				| (cast(uint)func << 8)
-				| (cast(uint)0x80000000); // the last value is to set enable bit
+				| (cast(uint)0x80000000);
 	}
 
 	ubyte headerType(uint address) {
