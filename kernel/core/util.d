097be9cdb7c86384da0ce2f3a032b77bef49180f
wolfwood <transmethyl@gmail.com>
2010-12-20 17:30:44 -0500

    refactor paging table code to user (shared) directory.  completely ignore architecture dependence

--- kernel/core/util.d
+++ kernel/core/util.d
@@ -28,40 +28,7 @@ template Tuple(T...)
 	alias T Tuple;
 }
 
-template Bitfield(alias data, Args...)
-{
-	static assert(!(Args.length & 1), "Bitfield arguments must be an even number");
-	const char[] Bitfield = BitfieldShim!((typeof(data)).stringof, data, Args).Ret;
-}
 
-// Odd bug in D templates -- putting "data.stringof" as a template argument gives it the
-// string of the type, rather than the string of the symbol.  This shim works around that.
-template BitfieldShim(char[] typeStr, alias data, Args...)
-{
-	const char[] Name = data.stringof;
-	const char[] Ret = BitfieldImpl!(typeStr, Name, 0, Args).Ret;
-}
-
-template BitfieldImpl(char[] typeStr, char[] nameStr, int offset, Args...)
-{
-	static if(Args.length == 0)
-		const char[] Ret = "";
-	else
-	{
-		const Name = Args[0];
-		const Size = Args[1];
-		const Mask = Bitmask!(Size);
-
-		const char[] Getter = "public " ~ typeStr ~ " " ~ Name ~ "() { return ( " ~
-			nameStr ~ " >> " ~ Itoh!(offset) ~ " ) & " ~ Itoh!(Mask) ~ "; }";
-
-		const char[] Setter = "public void " ~ Name ~ "(" ~ typeStr ~ " val) { " ~
-			nameStr ~ " = (" ~ nameStr ~ " & " ~ Itoh!(~(Mask << offset)) ~ ") | ((val & " ~
-			Itoh!(Mask) ~ ") << " ~ Itoh!(offset) ~ "); }";
-
-		const char[] Ret = Getter ~ Setter ~ BitfieldImpl!(typeStr, nameStr, offset + Size, Args[2 .. $]).Ret;
-	}
-}
 
 template Itoa(long i)
 {
@@ -89,10 +56,6 @@ template IntToStr(ulong i, int base)
 		const char[] IntToStr = "" ~ Digits!(base)[i % base];
 }
 
-template Bitmask(long size)
-{
-	const long Bitmask = (1L << size) - 1;
-}
 
 template isStringType(T)
 {
