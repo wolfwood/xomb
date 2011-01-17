9c809adfbdcf9d3823fb4331c6527f11ebb80088
wolfwood <transmethyl@gmail.com>
2011-01-17 01:59:14 -0500

    it's good to check for errors... so I realize when I break stuff.

--- build.sh
+++ build.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 cd runtimes/mindrt
-make
+make || exit
 cd ../..
 
+cd app/d/hello
+dsss clean && dsss build || exit
+cd ../../..
+
 cd app/d/posix
-dsss clean && dsss build
+dsss clean && dsss build || exit
 cd ../../..
 
 cd app/d/xsh
-dsss clean && dsss build
+dsss clean && dsss build || exit
 cd ../../..
 
 cd app/d/init
-dsss clean && dsss build
+dsss clean && dsss build || exit
 cd ../../..
 
 
 cd build
-dsss clean && dsss build
+dsss clean && dsss build || exit
 
 bochs -q
