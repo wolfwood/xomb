ff882e466fc0e30f0a97cd3944a8ab948320ee2b
wolfwood <wolfwood@xomb.org>
2011-09-28 20:13:47 -0400

    remove stupid dsss.last

--- build.sh
+++ build.sh
@@ -21,23 +21,23 @@ make || exit
 cd ../../..
 
 cd app/d/hello
-dsss clean && dsss build || exit
+dsss clean && rm dsss.last && dsss build || exit
 cd ../../..
 
 cd app/d/posix
-dsss clean && dsss build || exit
+dsss clean && rm dsss.last && dsss build || exit
 cd ../../..
 
 cd app/d/xsh
-dsss clean && dsss build || exit
+dsss clean && rm dsss.last && dsss build || exit
 cd ../../..
 
 cd app/d/init
-dsss clean && dsss build || exit
+dsss clean && rm dsss.last && dsss build || exit
 cd ../../..
 
 
 cd build
-dsss clean && dsss build || exit
+dsss clean && rm dsss.last && dsss build || exit
 
 bochs -q
