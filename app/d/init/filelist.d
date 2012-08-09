87fedbe2b7f05122445032d465d042ff0c06beb2
wolfwood <wolfwood@xomb.org>
2012-08-09 17:53:08 -0400

    scripty things

--- app/d/init/filelist.d
+++ app/d/init/filelist.d
@@ -1,14 +1,10 @@
 module filelist;
 import embeddedfs;
 void fileList(){
-	EmbeddedFS.makeFile!("binaries/hello")();
-	EmbeddedFS.makeFile!("binaries/example")();
-	EmbeddedFS.makeFile!("binaries/chel")();
-	EmbeddedFS.makeFile!("binaries/lspci")();
-	EmbeddedFS.makeFile!("binaries/dynhello")();
-	EmbeddedFS.makeFile!("binaries/simplymm")();
-	EmbeddedFS.makeFile!("binaries/posix")();
-	EmbeddedFS.makeFile!("binaries/xsh")();
 	EmbeddedFS.makeFile!("data/pci.ids")();
+	EmbeddedFS.makeFile!("binaries/lspci")();
+	EmbeddedFS.makeFile!("binaries/posix")();
+	EmbeddedFS.makeFile!("binaries/nettest")();
+	EmbeddedFS.makeFile!("binaries/xsh")();
 	EmbeddedFS.makeFile!("LICENSE")();
 }
